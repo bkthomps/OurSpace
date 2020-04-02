12a3c2f4e941821e43e3768d516242117d828663
bkthomps <bkthomps@edu.uwaterloo.ca>
2020-04-02 18:04:43 -0400

    Add account queries

---
+++ account.sql
@@ -0,0 +1,117 @@
+USE our_space;
+
+DROP PROCEDURE IF EXISTS CreateAccount;
+DELIMITER $$
+CREATE PROCEDURE CreateAccount(
+    IN first_name  VARCHAR(50),
+    IN last_name   VARCHAR(50),
+    IN sex_id      INT,
+    IN email       VARCHAR(50),
+    IN passcode    VARCHAR(50),
+    OUT person_id  BIGINT,
+    OUT error_code INT
+)
+BEGIN
+    DECLARE current_person_id BIGINT;
+    DECLARE email_count INT;
+    SELECT MAX(person.person_id) + 1 INTO current_person_id FROM person;
+    SELECT COUNT(person.email) INTO email_count FROM person WHERE person.email = email;
+    SET person_id = 0;
+    IF first_name = NULL OR CHAR_LENGTH(first_name) = 0 THEN
+        SET error_code = 1;
+    ELSEIF last_name = NULL OR CHAR_LENGTH(last_name) = 0 THEN
+        SET error_code = 2;
+    ELSEIF sex_id = NULL OR sex_id < 1 OR sex_id > 3 THEN
+        SET error_code = 3;
+    ELSEIF email = NULL OR CHAR_LENGTH(email) = 0 THEN
+        SET error_code = 4;
+	ELSEIF email_count != 0 THEN
+        SET error_code = 5;
+    ELSEIF passcode = NULL OR CHAR_LENGTH(passcode) < 8 THEN
+        SET error_code = 6;
+    ELSE
+        INSERT INTO person VALUES (current_person_id, first_name, last_name, sex_id, email, passcode);
+        SET error_code = 0;
+        SET person_id = current_person_id;
+    END IF;
+END$$
+DELIMITER ;
+
+CALL CreateAccount('Cave', 'Johnson', 1, 'cave.johnson@aperture.org', 'glados84', @person_id, @error_code);
+SELECT @person_id;
+SELECT @error_code;
+SELECT * FROM person WHERE person_id = 10216646959432839;
+
+DROP PROCEDURE IF EXISTS DeleteAccount;
+DELIMITER $$
+CREATE PROCEDURE DeleteAccount(
+    IN person_id   BIGINT,
+    OUT error_code INT
+)
+BEGIN
+    DECLARE user_count BIGINT;
+    SELECT COUNT(person.person_id) INTO user_count FROM person WHERE person.person_id = person_id;
+    IF user_count = 0 THEN
+		SET error_code = 1;
+	ELSE
+		-- TODO: remove all reactions
+        -- TODO: remove all comment replies
+        -- TODO: remove all post comments
+        -- TODO: remove all posts
+        -- TODO: unfollow all groups
+        -- TODO: unfollow all friends
+        -- TODO: remove all admins/deleting groups
+		DELETE FROM person WHERE person.person_id = person_id;
+		SET error_code = 0;
+    END IF;
+END$$
+DELIMITER ;
+
+-- CALL DeleteAccount(10216646959432839, @error_code);
+-- SELECT @error_code;
+-- SELECT * FROM person WHERE person_id = 10216646959432839;
+
+DROP PROCEDURE IF EXISTS ModifyAccount;
+DELIMITER $$
+CREATE PROCEDURE ModifyAccount(
+    IN person_id   BIGINT,
+    IN first_name  VARCHAR(50),
+    IN last_name   VARCHAR(50),
+    IN sex_id      INT,
+    IN email       VARCHAR(50),
+    IN passcode    VARCHAR(50),
+    OUT error_code INT
+)
+BEGIN
+	DECLARE email_count INT;
+    DECLARE person_count INT;
+    SELECT COUNT(person.email) INTO email_count FROM person WHERE person.email = email AND person.person_id != person_id;
+    SELECT COUNT(person.person_id) INTO person_count FROM person WHERE person.person_id = person_id;
+    IF person_count = 0 THEN
+        SET error_code = 7;
+    ELSEIF first_name = NULL OR CHAR_LENGTH(first_name) = 0 THEN
+        SET error_code = 1;
+    ELSEIF last_name = NULL OR CHAR_LENGTH(last_name) = 0 THEN
+        SET error_code = 2;
+    ELSEIF sex_id = NULL OR sex_id < 1 OR sex_id > 3 THEN
+        SET error_code = 3;
+    ELSEIF email = NULL OR CHAR_LENGTH(email) = 0 THEN
+        SET error_code = 4;
+	ELSEIF email_count != 0 THEN
+        SET error_code = 5;
+    ELSEIF passcode = NULL OR CHAR_LENGTH(passcode) < 8 THEN
+        SET error_code = 6;
+    ELSE
+		UPDATE person
+			SET person.first_name = first_name, person.last_name = last_name,
+				person.sex_id = sex_id, person.email = email, person.passcode = passcode
+			WHERE person.person_id = person_id;
+        SET error_code = 0;
+    END IF;
+END$$
+DELIMITER ;
+
+CALL ModifyAccount(10216646959432839, 'Cave', 'Johnson', 1, 'cave.johnson@aperture.org', 'glados92', @error_code);
+SELECT @person_id;
+SELECT @error_code;
+SELECT * FROM person WHERE person_id = 10216646959432839;
