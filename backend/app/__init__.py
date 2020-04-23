a1f70ac54a095abe0b476b9fe6469990148e57f0
Peiyao Chen <p72chen@uwaterloo.ca>
2020-04-22 23:06:29 -0400

    Unfollow and follow for friends

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,25 +1,79 @@
 import mysql.connector
-from flask import Flask
+from flask import Flask, request
 from os import environ
-from .utils.queries import groups_query
+from string import Template
+from .utils.queries import groups_query, show_followed_users, show_friend, show_unfollowed_users
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
 
-    # a simple page that says hello
-    @app.route('/groups')
-    def get_groups():
+    # user viewing and following users 
+    @app.route('/<uid>/users/follow', methods = ['GET', 'POST'])
+    def follow_user(uid):
         cnx = mysql.connector.connect(user=environ.get('DB_USER'), password=environ.get('DB_PASSWORD'), host=environ.get('DB_HOST'), database=environ.get('DB_NAME'), auth_plugin='mysql_native_password')
         cursor = cnx.cursor()
-        cursor.execute(groups_query)
         output = {}
-        for (group_id, group_name) in cursor:
-            output[group_id] = group_name
+        if request.method == 'POST':
+            friend_id = request.get_json()['id']
+            result = cursor.callproc('FollowFriend', [uid, friend_id, 0])
+            if result[-1] == 0:
+                cnx.commit()
+                cursor.execute(Template(show_friend).substitute(id=friend_id))
+                data = {}
+                for (person_id, first_name, last_name) in cursor:
+                    data[person_id] = {
+                        "userId": person_id,
+                        "firstName": first_name,
+                        "lastName": last_name, 
+                        }
+                output = {"data": data} 
+            else:
+                output = {"error": result[-1]}
+        else:
+            data = {}
+            cursor.execute(Template(show_followed_users).substitute(id=uid))
+            for (person_id, first_name, last_name) in cursor:
+                data[person_id] = {
+                    "userId": person_id,
+                    "firstName": first_name,
+                    "lastName": last_name, 
+                    }
+            output = {"data" : data}
         cursor.close()
         cnx.close()
-        return {
-            "groups": output
-        }
+        return output
 
+    # # user viewing and unfollowing friends
+    @app.route('/<uid>/users/unfollow', methods = ['GET','POST'])
+    def unfollow_user(uid):
+        cnx = mysql.connector.connect(user=environ.get('DB_USER'), password=environ.get('DB_PASSWORD'), host=environ.get('DB_HOST'), database=environ.get('DB_NAME'), auth_plugin='mysql_native_password')
+        cursor = cnx.cursor()
+        output = {}
+        if request.method == 'POST':
+            friend_id = request.get_json()['id']
+            result = cursor.callproc('UnfollowFriend', [uid, friend_id, 0])
+            if result[-1] == 0:
+                cnx.commit()
+                data = {
+                    "userId": friend_id
+                }
+                output = {"data": data}
+            else:
+                output = {"error": result[-1]}
+        else:
+            data = {}
+            cursor.execute(Template(show_unfollowed_users).substitute(id=uid))
+            for (person_id, first_name, last_name) in cursor:
+                data[person_id] = {
+                    "userId": person_id,
+                    "firstName": first_name,
+                    "lastName": last_name, 
+                    }
+            output = {"data" : data}
+        cursor.close()
+        cnx.close()
+        return output
+
+ 
     return app
--- backend/app/utils/queries.py
+++ backend/app/utils/queries.py
@@ -1,1 +1,4 @@
 groups_query = 'select * from our_space.`groups`'
+show_unfollowed_users =  'select person_id, first_name, last_name FROM our_space.person where not exists (select * from our_space.following_friends where first_person_id=$id and second_person_id = person_id) limit 100;'
+show_followed_users = 'select distinct second_person_id as person_id, first_name, last_name FROM our_space.following_friends inner join our_space.person on person_id=second_person_id where first_person_id = $id;'
+show_friend = 'select person_id, first_name, last_name from our_space.person where person_id=$id;'
